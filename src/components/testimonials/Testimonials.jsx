f4d82f5c1cb97545caa4ffe5b6501bb455917906
Gayanath Lakmevan Silva <gayanathlakmevansilva@gmail.com>
2024-01-14 19:08:37 +0530

    skills_updated

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -45,17 +45,21 @@ const Testimonials = () => {
           <div className="client_avatar">
             <img src={AVTR2} alt="Avatar One" />
           </div>
-          <h5 className="client_name">---------------------</h5>
-          <small className="client_review">Good communication skills </small>
+          <h5 className="client_name">Problem-Solving</h5>
+          <small className="client_review">
+            Strong problem-solving skills to analyze issues, identify root
+            causes, and implement effective solutions.
+          </small>
         </SwiperSlide>
 
         <SwiperSlide className="testimonial">
           <div className="client_avatar">
             <img src={AVTR3} alt="Avatar One" />
           </div>
-          <h5 className="client_name">---------------------</h5>
+          <h5 className="client_name">Critical Thinking</h5>
           <small className="client_review">
-            Ability to work under tight schedule
+            Capacity to think critically and assess situations from multiple
+            perspectives, leading to well-informed decisions.
           </small>
         </SwiperSlide>
 
@@ -63,8 +67,41 @@ const Testimonials = () => {
           <div className="client_avatar">
             <img src={AVTR4} alt="Avatar One" />
           </div>
-          <h5 className="client_name">---------------------</h5>
-          <small className="client_review">Team work</small>
+          <h5 className="client_name">Team Collaboration</h5>
+          <small className="client_review">
+            Ability to work collaboratively in a team environment, share
+            knowledge, and contribute to collective goals.
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
+          <div className="client_avatar">
+            <img src={AVTR4} alt="Avatar One" />
+          </div>
+          <h5 className="client_name">Adaptability</h5>
+          <small className="client_review">
+            Flexibility to adapt to changing project requirements, technologies,
+            and methodologies.
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
+          <div className="client_avatar">
+            <img src={AVTR4} alt="Avatar One" />
+          </div>
+          <h5 className="client_name">Creativity</h5>
+          <small className="client_review">
+            Creative thinking to devise innovative solutions and approaches to
+            software development challenges.
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
+          <div className="client_avatar">
+            <img src={AVTR4} alt="Avatar One" />
+          </div>
+          <h5 className="client_name">Time Management</h5>
+          <small className="client_review">
+            Effective time management skills to prioritize tasks, meet
+            deadlines, and manage workloads efficiently.
+          </small>
         </SwiperSlide>
       </Swiper>
     </section>
