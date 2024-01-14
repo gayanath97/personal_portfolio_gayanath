2e509e3de04f6b5822a3489f772273ca00318c8d
Gayanath Lakmevan Silva <gayanathlakmevansilva@gmail.com>
2024-01-14 17:37:12 +0530

    info_updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ML from "../../assets/mer.png";
+import ML from "../../assets/mer2.png";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
